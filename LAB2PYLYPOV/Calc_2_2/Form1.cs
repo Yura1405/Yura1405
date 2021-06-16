0d0a8431ebe92bbacedf14d6f36b2d91ff356277
Yurii <59280851+Yura1405@users.noreply.github.com>
2021-06-16 15:23:29 +0300

    Add files via upload

---
+++ LAB2PYLYPOV/Calc_2_2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc_2_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(textBox1.Text);
+            textBox1.Text = "";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            textBox1.Text = "";
+        }
+    }
+}
